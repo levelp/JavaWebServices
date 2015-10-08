fc136b16f3182b3615f84a40734bd52c7de4949f
Denis Stepulenok <super.denis@gmail.com>
2015-10-09 01:26:09 +0300

    REST-интерфейс на занятии

--- src/main/java/rest/GreetingController.java
+++ src/main/java/rest/GreetingController.java
@@ -1,9 +1,13 @@
 package rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -21,8 +25,9 @@ public class GreetingController {
         return "GREAT, Application started! :)";
     }
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @RequestMapping(value = "/greeting", produces = MediaType.APPLICATION_ATOM_XML_VALUE)
+    public Greeting greeting(@RequestParam(value = "name",
+            defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
@@ -35,9 +40,48 @@ public class GreetingController {
      * @param b второе слагаемое
      * @return сумма
      */
-    @RequestMapping("/add")
-    public String greeting(@RequestParam(value = "a", defaultValue = "2") int a,
-                           @RequestParam(value = "b", defaultValue = "2") int b) {
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(@RequestParam(value = "a", defaultValue = "2") int a,
+                      @RequestParam(value = "b", defaultValue = "2") int b) {
         return String.format("%d + %d = %d", a, b, a + b);
+    }
+
+    /**
+     * Сложение двух целых чисел
+     * Пример запроса: http://localhost:8080/add/3/10
+     * Пример ответа:  3 + 10 = 13
+     *
+     * @param as первое слагаемое
+     * @param bs второе слагаемое
+     * @return сумма
+     */
+    @RequestMapping(value = "/add/{a}/{b}")
+    public String add2(@PathVariable(value = "a") String as,
+                       @PathVariable(value = "b") String bs) throws IOException {
+        int a, b;
+        try {
+            a = Integer.parseInt(as);
+        } catch (NumberFormatException e) {
+            return "Первое значение '" + as + "' не является целым числом";
+        }
+        try {
+            b = Integer.parseInt(bs);
+        } catch (NumberFormatException e) {
+            return "Второе значение '" + bs + "' не является целым числом";
+        }
+        String res = String.format("%d + %d = %d", a, b, a + b);
+        try (PrintWriter writer = new PrintWriter(new FileWriter("log.txt", true))) {
+            writer.println(res);
+        }
+        return res;
+    }
+
+    @RequestMapping(value = "/search/{query}")
+    public List<String> search(@PathVariable(value = "query") String query) {
+        List<String> list = new ArrayList<>();
+        list.add("Поиск по строке '" + query + "'");
+        list.add("Найдено 1");
+        list.add("Найдено 2");
+        return list;
     }
 }
